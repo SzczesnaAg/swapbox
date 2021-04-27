c898663e8a53496c469528c256265ded0ea58205
Ola Hola <homoncik.aleksandra@gmail.com>
2021-04-27 17:22:05 +0200

    add a sticky map, disappearing after riching the breakpoint

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,22 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @markers = @products.geocoded.map do |product|
+      if product.category == 'Book'
+        {
+          lat: product.latitude,
+          lng: product.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { product: product }),
+          image_url: helpers.asset_url('book-black.png')
+        }
+      else
+        {
+          lat: product.latitude,
+          lng: product.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { product: product }),
+          image_url: helpers.asset_url('puzzle-piece.png')
+        }
+      end
+    end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,10 @@ class Product < ApplicationRecord
   validates :city, presence: true
   validates :street, presence: true
 
-  geocoded_by :city
-  after_validation :geocode, if: :will_save_change_to_city?
+  def address
+    "#{street}, #{zipcode} #{city}"
+  end
+
+  geocoded_by :address
+  after_validation :geocode, if: [:will_save_change_to_street?, :will_save_change_to_zipcode?, :will_save_change_to_city?]
 end
