f4f4976fcdbfae4cda695ab5661ab1c0a9d4bcdb
SzczesnaAg <agata.szczesna89@gmail.com>
2021-04-28 12:53:03 +0200

    first layout my dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,17 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :index]
+  skip_before_action :authenticate_user!, only: [:home, :index, :show]
 
   def home
   end
 
   def index
+  end
+
+  def show
+  end
+
+  def my_dashboard
+    @user = current_user
+    @products = Product.where(user_id: current_user.id)
   end
 end
