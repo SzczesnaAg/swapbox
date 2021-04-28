58384049aaf60f06e52c4b1fe9a6dee8f49a5895
Ola Hola <homoncik.aleksandra@gmail.com>
2021-04-28 16:28:01 +0200

    add swap model

--- app/controllers/swaps_controller.rb
+++ app/controllers/swaps_controller.rb
@@ -6,15 +6,38 @@ class SwapsController < ApplicationController
     @swap.product = @product
     @swap.user = current_user
     if user_signed_in?
-      @product.status = "onhold"
-      @product.save
-      @swap.save
-
-      redirect_to products_path, notice: "Your swap request has been successfully sent!"
+      if current_user.id == @product.user_id
+        redirect_to products_path, notice: "It's your product, swap unavailable!"
+      elsif current_user.products.where(status: 0).count.positive?
+        @product.status = "onhold"
+        @product.save
+        @swap.save
+        redirect_to products_path, notice: "Your swap request has been successfully sent!"
+      else
+        redirect_to products_path, notice: "You have no available products to swap!"
+      end
     else
       redirect_to new_user_session_url
     end
     authorize @swap
+  end
+
+  def sent_requests
+    @swaps = Swap.where(user_id: current_user.id)
+  end
+
+  def requests_for_owner
+    @swaps = Swap.products.where(user_id: current_user.id)
+  end
+
+  def mark_as_rejected
+    @swap = Swap.find(params[:id])
+    @swap.product = @product
+    @product.status = "available"
+    @product.save
+    @swap.status = "rejected"
+    @swap.save
+    redirect_to products_path, notice: "Swap request has been rejected!"
   end
 
   private
