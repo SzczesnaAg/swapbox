8e993abf164ed26acade46b036019117e9a2b878
Alexandra Boedtker <alexboedtker@gmail.com>
2021-04-29 13:06:01 +0200

    seeds for books, puzzles and adresses done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-
-  # scrap from restaurant database
-    # t.string "tags"
-    # t.string "street"
-    # t.integer "zipcode"
-    # t.string "city"
-
 require 'open-uri'
 require 'nokogiri'
 
@@ -27,7 +20,19 @@ def fetch_puzzle_urls
   puzzles
 end
 
-def scrape_puzzle(url)
+def get_addresses
+  url = "https://tel.search.ch/?was=Restaurant+&pages=9" #90 addresses here
+  html_file = open(url).read
+  html_doc = Nokogiri::HTML(html_file)
+  html_doc.search('.tel-address').map do |address|
+    street = address.text.split(',')[0]
+    zipcode = address.text.split(',')[1].split[0]
+    city = address.text.split(',')[1].split[1]
+    {street: street, zipcode: zipcode, city: city}
+  end
+end
+
+def scrape_puzzle(url, address)
   html_file = open(url, 'Accept-Language'=>'en').read
   html_doc = Nokogiri::HTML(html_file)
   title = html_doc.search('.productView-title').text
@@ -38,18 +43,58 @@ def scrape_puzzle(url)
     title: title,
     category: "Puzzle",
     description: description,
-    street: "Bahnhofstrasse 5",
-    zipcode: 8001,
-    city: "Zurich",
-    user: User.last
+    street: address[:street],
+    zipcode: address[:zipcode],
+    city: address[:city],
+    user: User.first
     )
+  file = URI.open(image)
+  product.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
   product.save!
 end
 
 urls = fetch_puzzle_urls
 
 user = User.create!(email: "sansa@north.com", last_name: "Stark", first_name: "Sansa", password: "123456")
+user = User.create!(email: "arya@north.com", last_name: "Stark", first_name: "Arya", password: "123456")
+addresses = get_addresses
+book_addresses = addresses.reverse
 
-puzzles = urls.each do |url|
-  scrape_puzzle(url)
+puzzles = urls.each_with_index do |url, index|
+  scrape_puzzle(url, addresses[index])
 end
+
+puts "Puzzles created"
+puts "Scraping books"
+
+def scrape_books(index, address)
+  url = "https://www.nytimes.com/books/best-sellers/" #has 55 books
+  html_file = open(url, 'Accept-Language'=>'en').read
+  html_doc = Nokogiri::HTML(html_file)
+  books = []
+  book_title = html_doc.search('.css-i1z3c1')[index].text
+  book_author = html_doc.search('.css-1nxjbfc')[index].text
+  title = book_title + ' ' + book_author
+  description = html_doc.search('.css-5yxv3r')[index].text
+  image = html_doc.search('.css-35otwa')[index].attribute('src').value
+  books << {title: title, description: description, image: image}
+
+  product = Product.new(
+  title: title,
+  category: "Book",
+  description: description,
+  street: address[:street],
+  zipcode: address[:zipcode],
+  city: address[:city],
+  user: User.last
+  )
+  file = URI.open(image)
+  product.photo.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+  product.save!
+end
+
+books = book_addresses.each_with_index do |address, index|
+  scrape_books(index, address) unless index > 20
+end
+
+puts "Books created"
