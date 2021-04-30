ee799b61d0d7d188640c18883983a9e17b4c4f04
Alexandra Boedtker <alexboedtker@gmail.com>
2021-04-30 16:34:14 +0200

    fixed timezone

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Swapbox
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Bern'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
