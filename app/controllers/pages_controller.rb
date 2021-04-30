04f2769972ec55143a9bd4044b44ad08a90cc9b3
SzczesnaAg <agata.szczesna89@gmail.com>
2021-04-30 14:58:59 +0200

    animation on team member photo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: [:home, :index, :show, :faq]
+  skip_before_action :authenticate_user!, only: [:home, :index, :show, :faq, :team]
 
   def home
   end
