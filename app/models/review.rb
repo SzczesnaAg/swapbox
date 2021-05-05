a515820adadbd933c9ec518214576697cfd8a0ab
Alexandra Boedtker <alexboedtker@gmail.com>
2021-05-05 12:22:30 +0200

    fixed layout of user showpage

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true
-  validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+  validates :rating, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+
+  default_scope { order(created_at: :desc) }
 end
