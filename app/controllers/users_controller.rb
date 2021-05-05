aec28530fee041663aef2705100d19967a618b8d
Alexandra Boedtker <alexboedtker@gmail.com>
2021-05-05 11:52:03 +0200

    added available products and organized reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < ApplicationController
     authorize @user
     @review = Review.new
     @reviews = Review.where(user: @user)
+    @products = Product.where(user_id: @user.id, status: 0)
   end
 end
