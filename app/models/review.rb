63df42bbc3cc64d5b63f3b8e2ab6d3c974f2122b
Alexandra Boedtker <alexboedtker@gmail.com>
2021-05-06 14:41:58 +0200

    fixed review error

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+  validates :rating, presence: true, inclusion: { in: 1..5 }, numericality: { only_integer: true }
 
   default_scope { order(created_at: :desc) }
 end
